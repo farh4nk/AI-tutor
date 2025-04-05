794f7dbe2e506bb03a41f9da21c654b930769d34
fkhan41 <fkhan41@stuy.edu>
2025-04-05 16:29:52 -0400

    updated pagebody

--- app/_components/PageBody.jsx
+++ app/_components/PageBody.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PageBody() {
   return (
-    <section className="bg-gray mt-20 lg:grid lg:h-screen lg:place-content-center">
+    <section className="bg-gray -mt-20 lg:grid lg:h-screen lg:place-content-center">
   <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
     <div className="mx-auto max-w-prose text-center">
       <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
